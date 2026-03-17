805165be61378bfbed0ba8ba14a1d96dbc34436f
Dima Shved <dmitriy.shved0@gmail.com>
2026-03-17 20:32:25 +0100

    after create hook

---
+++ internal/resources/modifier/hooks.go
@@ -0,0 +1,36 @@
+package modifier
+
+import (
+	"context"
+
+	"uber-go-menu/internal/domain"
+	"uber-go-menu/internal/pkg/errorx"
+	"uber-go-menu/internal/platform/crud"
+)
+
+type Hooks struct {
+	crud.NoopHooks[domain.Modifier, CreateRequest, UpdateRequest, domain.Modifier]
+}
+
+func (Hooks) AfterCreate(ctx context.Context, hookCtx crud.HookContext, request *CreateRequest, entity *domain.Modifier) error {
+	if len(request.Options) == 0 {
+		return nil
+	}
+
+	options := make([]domain.ModifierOption, 0, len(request.Options))
+	for _, option := range request.Options {
+		options = append(options, domain.ModifierOption{
+			Name:         option.Name,
+			Price:        option.Price,
+			MaxSelection: option.MaxSelection,
+			IsAvailable:  option.IsAvailable,
+			ModifierID:   entity.ID,
+		})
+	}
+
+	if err := hookCtx.Tx.WithContext(ctx).Create(&options).Error; err != nil {
+		return errorx.ErrDatabaseQuery.WithDetails(err.Error())
+	}
+	entity.Options = options
+	return nil
+}

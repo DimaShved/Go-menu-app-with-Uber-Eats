eb7b280b723a4494efd98fa07623d82ef76d4dd5
Dima Shved <dmitriy.shved0@gmail.com>
2026-03-17 21:27:00 +0100

    variation dto

---
+++ internal/resources/variation/dto.go
@@ -0,0 +1,20 @@
+package variation
+
+import "github.com/google/uuid"
+
+type OptionRequest struct {
+	Name        string `json:"name" validate:"required"`
+	Price       int    `json:"price" validate:"required"`
+	IsAvailable bool   `json:"is_available" validate:"boolean,omitempty"`
+}
+
+type CreateRequest struct {
+	Name       string          `json:"name" validate:"required,min=2,max=255"`
+	CategoryID uuid.UUID       `json:"category_id" validate:"required"`
+	Options    []OptionRequest `json:"options" validate:"omitempty,min=1,dive"`
+}
+
+type UpdateRequest struct {
+	Name       string    `json:"name" validate:"required,min=2,max=255"`
+	CategoryID uuid.UUID `json:"category_id" validate:"required"`
+}

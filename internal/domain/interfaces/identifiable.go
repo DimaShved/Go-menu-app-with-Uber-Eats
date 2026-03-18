9e07894c074b03e6d3b26f94e544c0103b273a5e
Dima Shved <dmitriy.shved0@gmail.com>
2026-03-18 18:47:27 +0100

    domain/interfaces deleted

--- internal/domain/interfaces/identifiable.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "github.com/google/uuid"
-
-type Identifiable interface {
-	GetID() uuid.UUID
-}
--- internal/domain/interfaces/preloader.go
+++
@@ -1,5 +0,0 @@
-package interfaces
-
-type Preloader interface {
-	PreloadRelations() []string
-}

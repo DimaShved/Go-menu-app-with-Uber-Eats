a6de9754ff0c6642af0b238986295fa0fbb2b600
Dima Shved <dmitriy.shved0@gmail.com>
2026-03-19 19:21:16 +0100

    Made standard menu availability create a single-record CRUD create

--- internal/resources/menu_availability/dto.go
+++ internal/resources/menu_availability/dto.go
@@ -9,13 +9,15 @@ type AvailabilityRequest struct {
 }
 
 type CreateRequest struct {
-	MenuSectionID  uuid.UUID             `json:"menu_section_id" validate:"required"`
-	Availabilities []AvailabilityRequest `json:"availabilities" validate:"required,min=1,dive"`
-}
-
-type UpdateRequest struct {
 	MenuSectionID uuid.UUID `json:"menu_section_id" validate:"required"`
 	DayOfWeek     int       `json:"day_of_week" validate:"required,oneof=1 2 3 4 5 6 7"`
 	OpenTime      int       `json:"open_time" validate:"gte=0,lte=1439"`
 	CloseTime     int       `json:"close_time" validate:"gte=0,lte=1439"`
 }
+
+type BatchUpsertRequest struct {
+	MenuSectionID  uuid.UUID             `json:"menu_section_id" validate:"required"`
+	Availabilities []AvailabilityRequest `json:"availabilities" validate:"required,min=1,dive"`
+}
+
+type UpdateRequest = CreateRequest
